3488316dddfffed6e45819b090048662020a81c1
Gregory Zimmer <gregory.zimmer@warren.com.br>
2022-09-09 12:27:23 -0300

    feat : Create BaseEntity and CustomerEntity

---
+++ ApiCustomer/Data/Data/Entity/BaseEntity.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Data.Entity
+{
+    public class BaseEntity
+    {
+        [Key]
+        public long Id { get; set; }
+    }
+}
---
+++ ApiCustomer/Data/Data/Entity/CustomerEntity.cs
@@ -0,0 +1,19 @@
+﻿namespace Data.Entity
+{
+    public class CustomerEntity : BaseEntity
+    {
+        public string FullName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string EmailConfirmation { get; set; } = string.Empty;
+        public string Cpf { get; set; } = string.Empty;
+        public string Cellphone { get; set; } = string.Empty;
+        public string Country { get; set; } = string.Empty;
+        public string City { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+        public string PostalCode { get; set; } = string.Empty;
+        public int Number { get; set; }
+        public bool EmailSms { get; set; }
+        public bool Whatsapp { get; set; }
+        public DateTime DateOfBirth { get; set; }
+    }
+}
