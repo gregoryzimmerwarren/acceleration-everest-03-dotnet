4f5933076c24410daf475f42a29bf6b84c36a201
Gregory Zimmer <gregory.zimmer@warren.com.br>
2022-09-16 14:23:36 -0300

    feat : Created the Profiles

---
+++ ApiCustomer/AppServices/Profiles/CustomerProfiles.cs
@@ -0,0 +1,16 @@
+﻿using AppModels.DTOs;
+using AutoMapper;
+using DomainModels.Models;
+
+namespace AppServices.Profiles
+{
+    public class CustomerProfiles : Profile
+    {
+        public CustomerProfiles()
+        {
+            CreateMap<CustomerModel, GetCustomerDto>();
+            CreateMap<PostCustomerDto, CustomerModel>();
+            CreateMap<PutCustomerDto, CustomerModel>();
+        }
+    }
+}
