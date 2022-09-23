7179c08b11a184d2b38d8e65219fee8b3fcb8827
Gregory Zimmer <gregory.zimmer@warren.com.br>
2022-09-23 13:55:15 -0300

    refactor : Removing unused imports

--- acceleration-everest-03-dotnet/Api/Program.cs
+++ acceleration-everest-03-dotnet/Api/Program.cs
@@ -10,7 +10,6 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
 using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
